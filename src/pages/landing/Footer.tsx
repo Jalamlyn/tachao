4008be8d3bf79c2b7119c95b5d3ee9c9015c6b56
kinop88 <112365362@qq.com>
2024-11-19 12:19:13 +0800

    feat(footer): add ICP registration number with link
    - Add ICP registration number '苏ICP备2024139328号' to footer
    - Add clickable link to https://beian.miit.gov.cn/#/Integrated/recordQuery
    - Maintain existing footer layout and functionality
    
    By: AI Developer Mo

--- src/pages/landing/Footer.tsx
+++ src/pages/landing/Footer.tsx
@@ -60,7 +60,7 @@ const Footer: React.FC = () => {
             <h3 className='text-white font-bold mb-4'>联系方式</h3>
             <ul className='space-y-2'>
               <li className='text-white/70'>邮箱：shihong@mobenai.com.cn</li>
-              <li className='text-white/70'>电话：15384078477</li>
+              <li className='text-white/70'>电话：13861608848</li>
               <li className='text-white/70'>地址：杭州市临平区</li>
             </ul>
           </div>
@@ -68,7 +68,7 @@ const Footer: React.FC = () => {
 
         <div className='mt-12 pt-8 border-t border-white/10'>
           <div className='flex flex-col md:flex-row justify-between items-center'>
-            <p className='text-white/70 text-sm'>© {currentYear} 沙塔智能. 保留所有权利.</p>
+            <p className='text-white/70 text-sm'>© {currentYear} 江苏神码投资有限公司. 保留所有权利.</p>
             <div className='flex space-x-4 mt-4 md:mt-0'>
               <Link href='#' className='text-white/70 hover:text-white text-sm'>
                 隐私政策
@@ -77,6 +77,15 @@ const Footer: React.FC = () => {
                 服务条款
               </Link>
             </div>
+          </div>
+          <div className='text-center mt-4'>
+            <Link 
+              href='https://beian.miit.gov.cn/#/Integrated/recordQuery' 
+              target='_blank'
+              className='text-white/50 hover:text-white/70 text-sm transition-colors'
+            >
+              苏ICP备2024139328号
+            </Link>
           </div>
         </div>
       </div>
