09002c579c719e397e50014ef0e8d939feeca743
kinop88 <112365362@qq.com>
2025-01-24 04:12:16 +0800

    refactor(vite.config): 更新 API 基础 URL
    - 将生产环境的 API URL 从腾讯云 API 网关变更为腾讯云服务端
    - 修改后的 URL 为 https://1259692580-b9dznk0gp5.na-siliconvalley.tencentscf.com/api
    - 开发环境的 API URL 保持不变，仍为 "/dev"

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
   define: {
-    __API_BASE_URL__: JSON.stringify(process.env.NODE_ENV === "production" ? "https://service-fpf07h2s-1259692580.usw.apigw.tencentcs.com/release/api" : "/dev"),
+    __API_BASE_URL__: JSON.stringify(process.env.NODE_ENV === "production" ? "https://1259692580-b9dznk0gp5.na-siliconvalley.tencentscf.com/api" : "/dev"),
   },
   plugins: [
     million.vite({ auto: true }),
